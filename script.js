11d7f2fd3573aabaec984f670ca73dab51690cad
Syed Muzammil <mrsyedmuzammil@gmail.com>
2025-07-25 15:50:52 +0500

    Add save button : Saves the counts and resets the counter to 0 RESET BUTTON : Resets both the counter and the previous counts ADD : Previous counts and Total Counts

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let countNum = document.getElementById("count");
+let savedCounts = document.getElementById("saved-counts");
+let savedTotal = document.getElementById("saved-total-prayers");
 
 let count = 0;
 
@@ -7,7 +9,19 @@ function increment() {
   countNum.innerHTML = count;
 }
 
+function resetCount() {
+  count = 0;
+  countNum.innerHTML = "0";
+}
+
+function save() {
+  let countStr = count + " - ";
+  savedCounts.textContent += countStr;
+  resetCount();
+}
+
 function reset() {
   count = 0;
   countNum.innerHTML = "0";
+  savedCounts.textContent = "Previous Prayers : ";
 }
